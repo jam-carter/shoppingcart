9053c5fc8ffba9f5d3133d7ea3b840524ad6b96a
j-ammaz <jamaulaaron259@gmail.com>
2023-10-18 15:24:06 +0800

    Third iteration changes

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,6 +1,16 @@
+import { services } from "../constants"
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <section className="max-container flex justify-center
+      flex-wrap gap-9">
+        {services.map((service) => (
+          <ServiceCard key={service.label} 
+          {...service} />
+        ))}
+      </section>
+    </div>
   )
 }
 
